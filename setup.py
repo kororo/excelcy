3cfd627cccc1006980e4fa1b56c45760e8edada7
Robertus Johansyah <robertus@johansyah.com>
2018-07-19 12:04:52 +1000

    Improve test experience Update setup

--- setup.py
+++ setup.py
@@ -1,23 +1,36 @@
+import os
 from io import open
-
 from setuptools import find_packages, setup
 
 
+def get_requirements(r: str):
+    try:  # for pip >= 10
+        from pip._internal.req import parse_requirements
+    except ImportError:  # for pip <= 9.0.3
+        from pip.req import parse_requirements
+
+    # parse_requirements() returns generator of pip.req.InstallRequirement objects
+    if os.path.exists(r):
+        install_reqs = parse_requirements(r, session=pkg)
+        return install_reqs
+    return []
+
+
 with open('excelcy/__init__.py', 'r') as f:
+    version = '0.0.1'
     for line in f:
         if line.startswith('__version__'):
             version = line.strip().split('=')[1].strip(' \'"')
             break
-    else:
-        version = '0.1.0'
 
 with open('README.rst', 'r', encoding='utf-8') as f:
     readme = f.read()
 
-REQUIRES = []
+pkg = 'excelcy'
+REQUIRES = [str(ir.req) for ir in get_requirements('requirements.txt') if ir.req]
 
 setup(
-    name='excelcy',
+    name=pkg,
     version=version,
     description='',
     long_description=readme,
@@ -38,7 +51,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: CPython',
@@ -47,6 +59,5 @@ setup(
 
     install_requires=REQUIRES,
     tests_require=['coverage', 'pytest'],
-
     packages=find_packages(),
 )
--- tests/test_data_trainer.py
+++ tests/test_data_trainer.py
@@ -1,28 +1,59 @@
-from excelcy import ExcelCy
+import os
+
+import spacy
+from excelcy import ExcelCy, DataTrainer
 from tests.test_base import BaseTestCase
 
 
 class TestDataTrainer(BaseTestCase):
-    def test_train(self):
-        # test: train with single row
-        # Annotate Uber manually to ORG
+    def test_clean(self):
+        # test: test the clean process
         test_path = self.get_test_data_path('test_data_01.xlsx')
         excelcy = ExcelCy()
         excelcy.train(data_path=test_path, options={'clean': True})
+        excelcy.train(data_path=test_path, options={'clean': True})
+
+    def test_missing_ner_pipe(self):
+        base = 'en_core_web_sm'
+        nlp = spacy.load(base)
+        # lets remove ner to simulate
+        nlp.remove_pipe('ner')
+
+        # save and reload to verify
+        name = 'nlp/test_data_01'
+        # create dir nlp
+        os.makedirs(name, exist_ok=True)
+        # save it
+        nlp.to_disk(name)
+
+        # lets try it
+        test_path = self.get_test_data_path('test_data_01.xlsx')
+        self.assert_ents(data_path=test_path)
+
+    def test_train_basic(self):
+        # test: train with single row
+        # Annotate Uber manually to ORG
+        test_path = self.get_test_data_path('test_data_01.xlsx')
+        self.assert_ents(data_path=test_path)
 
     def test_train_matcher_nlp(self):
         # test: train with pipe matcher
         # Annotate Uber with nlp pattern phrase matcher
         test_path = self.get_test_data_path('test_data_02.xlsx')
         tests = {'1': {('$1 million', 'MONEY'), ('a week', 'DATE')}}
-        self.assert_ents(data_path=test_path, options={'clean': True}, tests=tests)
+        self.assert_ents(data_path=test_path, tests=tests)
 
     def test_train_matcher_regex(self):
         # test: train with pipe matcher
         # Annotate aweek with regex pattern matcher
         test_path = self.get_test_data_path('test_data_03.xlsx')
         tests = {'1': {('Uber', 'ORG')}}
-        self.assert_ents(data_path=test_path, options={'clean': True}, tests=tests)
+        self.assert_ents(data_path=test_path, tests=tests)
+
+    def test_train_complex(self):
+        # test: train with more complex data including given span
+        test_path = self.get_test_data_path('test_data_04.xlsx')
+        self.assert_ents(data_path=test_path)
 
     def test_train_smash(self):
         # disabled for now, 2880 sentences test takes 1286 seconds
@@ -31,3 +62,22 @@ class TestDataTrainer(BaseTestCase):
         # test: train with 2880 sentences
         # test_path = self.get_test_data_path('test_data_05.xlsx')
         # self.assert_ents(data_path=test_path, options={'clean': True})
+
+    def test_read_write(self):
+        # read the data first
+        test_path = self.get_test_data_path('test_data_01.xlsx')
+        data_trainer = DataTrainer(data_path=test_path)
+
+        # prepare the dir
+        nlp_path = self.get_test_data_path('nlp/nlp')
+        os.makedirs(nlp_path, exist_ok=True)
+        # re-create the data
+        test_path2 = self.get_test_data_path('nlp/test_data_01.xlsx')
+        data_trainer.data_save(test_path2)
+
+        # reload it again
+        data_trainer2 = DataTrainer(data_path=test_path2)
+
+        # and compare them, it should be exact the same
+        assert data_trainer.data_train == data_trainer2.data_train
+        assert data_trainer.data_config == data_trainer2.data_config
