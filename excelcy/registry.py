09b222b39e0f421edc7264eaf5ec280e53e96fbf
Robertus Johansyah <robertus@johansyah.com>
2018-07-29 11:55:09 +1000

    Refactor current fns to adapt new phase API

--- excelcy/registry.py
+++ excelcy/registry.py
@@ -7,6 +7,29 @@ field = attr.ib
 
 @attr.s
 class Registry(object):
+    @classmethod
+    def field_names(cls):
+        names = [field.name for field in attr.fields(cls)]
+        return names
+
+    @classmethod
+    def __registry__filter_items(cls, items: dict) -> dict:
+        """
+        Filtered items based on the given class attrs
+        :param item: Data in dictionary
+        :return: Filtered data in dictionary
+        """
+        names = cls.field_names()
+        validated_items = {k: items.get(k) for k in set(names).intersection(set(items.keys()))}
+        return validated_items
+
+    @classmethod
+    def make(cls, items: dict):
+        filtered_items = cls.__registry__filter_items(items=items)
+        # noinspection PyArgumentList
+        inst = cls(**filtered_items)
+        return inst
+
     def __getattribute__(self, item):
         try:
             item = super(Registry, self).__getattribute__(item)
@@ -29,12 +52,12 @@ class Registry(object):
         return init_by_attr_s
 
     def extend(self, items: dict):
-        items = items if items else {}
-        for k, v in items.items():
+        filtered_items = self.__registry__filter_items(items=items)
+        for k, v in filtered_items.items():
             object.__setattr__(self, k, v)
         return self
 
-    def items(self):
+    def as_dict(self) -> dict:
         data = attr.asdict(self, dict_factory=collections.OrderedDict)
         return data
 
